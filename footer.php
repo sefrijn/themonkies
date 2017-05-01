d30a80e1f3a3875a15dc0f32decc57f402bdd893
Sefrijn <sefrijn@gmail.com>
2017-05-01 18:02:30 +0200

    One more footer update

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@ if(count($events) != 0){ ?>
 
 
 <footer>
-&copy; The Monkies <?php echo date("Y"); ?> | <span class="dashicons dashicons-email-alt"></span> <a href="mailto:love@themonkies.nl">love@themonkies.nl</a> | <span class="dashicons dashicons-facebook-alt"></span> <a href="http://www.facebook.com/themonkies">Facebook</a> | Site by <a href="http://www.sefrijn.nl/">Sefrijn</a>
+&copy; The Monkies <?php echo date("Y"); ?> | <span class="dashicons dashicons-email-alt"></span> <a href="mailto:love@themonkies.nl">love@themonkies.nl</a> | <span class="dashicons dashicons-facebook-alt"></span> <a href="http://www.facebook.com/themonkies">Facebook</a> | Site by <a href="http://www.howaboutyes.com/">How About Yes</a>
 </footer>
 <?php wp_footer(); ?>
 </body>
